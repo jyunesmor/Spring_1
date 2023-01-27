20635ebd1c48353aac7cd1faa6c856ca11ddd188
jyunesmor <109996179+jyunesmor@users.noreply.github.com>
2023-01-27 19:22:30 -0300

    Primera Carga 27-01-2023

---
+++ Ejercicio_Fundamental/Noticia/src/main/java/com/egg/Noticia/NoticiaApplication.java
@@ -0,0 +1,13 @@
+package com.egg.Noticia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiaApplication.class, args);
+	}
+
+}
